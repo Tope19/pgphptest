6670e9ee665eb84d0a396b2ddcaaf1ea1e738425
Tope Olotu <topeolotu75@gmail.com>
2021-06-05 01:34:44 +0100

    dispalyed the right data for the logged in user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<header>
+    <span class="avatar"><img src="{{ asset('assets/images/users/1.jpg') }}" alt="" /></span>
+    <h1>{{ $user->name }}</h1>
+    <p><{{ nl2br($user->comments) }}</p>
+</header>
 @endsection
