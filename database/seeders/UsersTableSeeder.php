d675aaef0bc03a94787139aa9dbebbb94b6e884f
Tope Olotu <topeolotu75@gmail.com>
2021-06-05 01:16:37 +0100

    modified the comments field to nullable, created a faactory file for users table

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        $usersQuantity = 5;
+
+        \App\Models\User::factory($usersQuantity)->count(30)->create();
+
+
+    }
+}
